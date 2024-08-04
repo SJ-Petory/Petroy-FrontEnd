2e3c2b4d86f7e73c4ddea38a37476739751cc697
김지훈 <wlgns5041@naver.com>
2024-08-05 01:39:18 +0900

    [feat #6] 메인페이지 초기세팅

---
+++ src/pages/mainPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/mainPage.css';
+
+function MainPage() {
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default MainPage;
+
+
+
