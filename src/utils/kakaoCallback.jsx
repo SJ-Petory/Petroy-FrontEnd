ec9645494ad38677a2704369d431d270ec94f4b5
김지훈 <wlgns5041@naver.com>
2024-08-22 18:31:38 +0900

    [feat #86] 바디에 email, phone 포함 전송

--- src/utils/kakaoCallback.jsx
+++ src/utils/kakaoCallback.jsx
@@ -8,34 +8,34 @@ function KakaoCallback() {
     const phone = localStorage.getItem('phone');
 
     useEffect(() => {
-      if (code && email && phone) {
-        fetch('http://43.202.195.199:8080/oauth/kakao/callback', {
-          method: 'POST', 
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ code, email, phone }),  
-        })
-        .then(response => response.json())
-        .then(data => {
-          if (data.success) {
-            alert("카카오 로그인 성공");
-            localStorage.removeItem('email');
-            localStorage.removeItem('phone');
-          } else {
-            alert("카카오 로그인 실패 : " + data.message);
-          }
-        })
-        .catch(error => {
-          console.error('에러 :', error);
-        });
-      }
+        if (code && email && phone) {
+            fetch('http://43.202.195.199:8080/oauth/kakao/callback', {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, phone }),  
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    alert("카카오 로그인 성공");
+                    localStorage.removeItem('email');
+                    localStorage.removeItem('phone');
+                } else {
+                    alert("카카오 로그인 실패 : " + data.message);
+                }
+            })
+            .catch(error => {
+                console.error('에러 :', error);
+            });
+        }
     }, [code, email, phone]);
 
     return (
-      <div>
-        <h1>카카오 로그인 중입니다.</h1>
-      </div>
+        <div>
+            <h1>카카오 로그인 중입니다.</h1>
+        </div>
     );
 }
 
