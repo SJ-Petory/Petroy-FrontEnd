23850cac746bc40f2c2b16cee05566a38a7f80ed
김지훈 <wlgns5041@naver.com>
2024-08-27 23:08:22 +0900

    [fix #86] css 경로수정

--- src/components/commons/PetRegister.jsx
+++ src/components/commons/PetRegister.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import '../styles/petPage.css';
+import '../../styles/PetPage.css';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
