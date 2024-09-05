de7390ffb121272f557a6af2bdfca267dbd84dc8
김지훈 <wlgns5041@naver.com>
2024-09-05 15:00:04 +0900

    [feat #86] 커뮤니티 페이지 네비바 구현

--- src/pages/Community/CommunityPage.jsx
+++ src/pages/Community/CommunityPage.jsx
@@ -1,7 +1,10 @@
+import NavBar from '../../components/commons/NavBar.jsx';
+
 const CommunityPage = () => {
 
     return (
         <div>
+            <NavBar title="커뮤니티" />
             <h2>커뮤니티 페이지임</h2>
         </div>
     )
