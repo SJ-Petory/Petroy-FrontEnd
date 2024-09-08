c3845952480d55307dc60e2c1bff47fe379371f4
김지훈 <wlgns5041@naver.com>
2024-09-08 22:44:38 +0900

    [feat #6] 일정 카테고리 생성 버튼 및 모달 추가

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,13 +1,22 @@
+import React, { useState } from 'react';
 import '../../styles/Main/MainPage.css';
 import CalendarComponent from '../../components/commons/CalendarComponent.jsx';
 import NavBar from '../../components/commons/NavBar.jsx';
+import CategoryModal from '../../components/Schedule/CategoryModal.jsx'
 
 function MainPage() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const openModal = () => setModalIsOpen(true);
+    const closeModal = () => setModalIsOpen(false);
 
     return (
         <div className="main-page">
             <NavBar title="메인페이지" />
             <CalendarComponent />
+            <button onClick={openModal} className="create-category-button">
+                일정 카테고리 생성
+            </button>
+            <CategoryModal isOpen={modalIsOpen} onRequestClose={closeModal} />
         </div>
     );
 }
